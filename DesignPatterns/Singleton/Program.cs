b54369ff7f692a6bd43ddf13c79ea2446dc8215a
TIEMPODEV\omar franco <ofranco@tiempodevelopment.com>
2021-05-16 11:46:30 -0600

    Singleton

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mustang = Car.GetInstance();
+
+            mustang.Motor = "V8";
+            var challenger = Car.GetInstance();
+
+            Console.Write(mustang.Motor);
+            Console.ReadLine();
+        }
+    }
+
+    internal class Car
+    {
+        private static Car _instance;
+        private Car() { }
+
+        public static Car GetInstance()
+        {
+            if (_instance != null)
+                return _instance;
+            return new Car();
+        }
+        
+        public string Motor { get; set; }
+        public string Transmission { get; set; }
+    }
+}
